ce0216321840dd3d909d0759008b24d01405e505
jywang99 <junyi.wang.007@gmail.com>
2022-06-26 02:51:49 +0900

    html title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, useSearchParams } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home/home';
 import Edit from './pages/edit/edit';
 import Admin from './pages/admin/admin';
